826fdf1c093d8ba00dd231738a34a81d02fc621e
Xiaotouming <ydm1996@protonmail.com>
2026-08-16 16:12:16 +0900

    fix-edge-runtime

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+
+export const runtime = "edge";
 import { notFound } from "next/navigation";
 import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { getTranslations, setRequestLocale } from "next-intl/server";
